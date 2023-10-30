f4fb5268ce691d5517ef4591747a4ad2742443d6
aurelie661 <boutaurel59@gmail.com>
2023-10-30 16:11:06 +0100

    exo2 tab contact

---
+++ my-first-project/src/app/models/Contact.model.ts
@@ -0,0 +1,14 @@
+class Contact{
+    public static count:0
+    id: number
+    constructor(
+        public firstName: string,
+        public lastName: string,
+        public age: number,
+        public email: string,
+        public phone: string,
+    ){
+        this.id = ++ Contact.count
+    }
+}
+export  default Contact
