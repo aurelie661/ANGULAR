fe3f0d1300d80e62096da0a66580c3163142e549
aurelie661 <boutaurel59@gmail.com>
2023-10-31 16:35:40 +0100

    exo5 done

--- my-first-project/src/app/components/list-element/list-element.component.ts
+++ my-first-project/src/app/components/list-element/list-element.component.ts
@@ -7,12 +7,12 @@ import Contact from '../../models/Contact.model'
   styleUrls: ['./list-element.component.css']
 })
 export class ListElementComponent {
+  newContact: Contact[] = []
   @Input()
   contacts: Contact[] = []
 
   onDelete(index: number){
     this.contacts.splice(index,1)
+    // this.contacts = [...this.contacts.filter(c => c !== contact)];  
+    }
   }
-
-
-}
