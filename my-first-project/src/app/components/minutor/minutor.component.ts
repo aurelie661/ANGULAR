737c748163e13c0192155aa36fedc0d25057072f
aurelie661 <boutaurel59@gmail.com>
2023-11-03 15:41:23 +0100

    exo8 in progress

--- my-first-project/src/app/components/minutor/minutor.component.ts
+++ my-first-project/src/app/components/minutor/minutor.component.ts
@@ -6,5 +6,17 @@ import { Component } from '@angular/core';
   styleUrls: ['./minutor.component.css']
 })
 export class MinutorComponent {
+  minutor=0
+  interval: any
+  isVisible=false
 
+  startMinutor(){
+    this.interval = setInterval(() => {
+      if(this.minutor > 0){
+      this.isVisible= true
+      this.minutor--
+        
+      }
+    },1000)
+  }
 }
